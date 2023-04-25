462d86ec1c685728d2510777fe728baeb25c3f97
Sonu Kumar Singh <iamersonusingh@gmail.com>
2023-04-25 17:17:10 +0530

    .

---
+++ prog.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std; 
+int main()
+{
+    int x = 0;
+    int y = 0;
+    cout<<"Enter the value of x : ";
+    cin>>x;
+    cout<<"Enter the value of y: ";
+    cin>>y;
+    int sum = x + y;
+    cout<<"Addition of two numbers are :"<<sum;
+
+    return 0;
+}
