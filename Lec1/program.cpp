6424eb98e12552457f5a17b145cd02f0fa687443
Sonu Kumar Singh <iamersonusingh@gmail.com>
2023-04-25 17:46:53 +0530

    .

--- Lec1/program.cpp
+++ Lec1/program.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int i=1;
+    // int i=1;
     // while(i<=100)
     // {
     //     if(i%2==0)
@@ -38,11 +38,61 @@ int main()
     // }
 
     //Factorial of number :
-    int fact  = 1; 
-    for(i=1;i<=5;i++)
-    {
-        fact = fact * i;
-    }
-    cout<<"Factorial of number is "<<fact;
+    // int fact  = 1; 
+    // for(i=1;i<=5;i++)
+    // {
+    //     fact = fact * i;
+    // }
+    // cout<<"Factorial of number is "<<fact;
+
+    //sum of digits
+
+    // int number = 0;
+    // int rem = 0;
+    // int sum = 0;
+    // cout<<"Enter the number : ";
+    // cin>>number;
+    // while(number>0)
+    // {
+    //     rem=number%10;
+    //     sum=sum+rem;
+    //     number = number / 10;
+    // }
+    // cout<<"Sum of digits are: "<<sum;
+
+    //Reverse of digits : 
+    // int number = 0;
+    // int rem = 0;
+    // int rev = 0;
+    // cout<<"Enter the number : ";
+    // cin>>number;
+    // while(number>0)
+    // {
+    //     rem=number%10;
+    //     rev=rev*10+rem;
+    //     number = number / 10;
+    // }
+    // cout<<"Reverse of digits are: "<<rev;
+
+    //plaindrome of number : 
+    // int number = 0;
+    // int rem = 0;
+    // int rev = 0;
+    // cout<<"Enter the number : ";
+    // cin>>number;
+    // int temp =  number;
+    // while(number>0)
+    // {
+    //     rem=number%10;
+    //     rev=rev*10+rem;
+    //     number = number / 10;
+    // }
+    // if(temp ==  rev)
+    // {
+    //     cout<<"Entered number is palindrome";
+    // }
+    // else{
+    //     cout<<"Not palindrome";
+    // }
     return 0;
 }
